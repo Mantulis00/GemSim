b43844c4973649d337ea61e8a8877dd677b11487
Mantulis00 <mantas.radzevicius1@gmail.com>
2020-06-18 07:59:44 +0300

    Initial commit

---
+++ Assets/Scripts/Spawn/SpawnerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void CreateLine(GameObject obModel)
+    {
+        GameObject go;
+        go = Instantiate(obModel) as GameObject;
+        go.transform.SetParent(this.transform);
+    }
+
+}
